6802ecdd6ea08e61a334544d1e14f1ba836e2e28
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2023-02-11 01:45:13 +1300

    Update install_yolov5.sh

--- install_yolov5.sh
+++ install_yolov5.sh
@@ -4,55 +4,24 @@ cd ~
 
 sudo apt update
 
-sudo apt install -y python3-pip
-pip3 install --upgrade pip
+sudo apt install -y python3-pip 
+python3 -m pip install --upgrade pip
 
-git clone https://github.com/ultralytics/yolov5
-
-cd yolov5
-
-cp requirements.txt requirements.txt.bkup
-
-sed -i 's/^torch>=1.7.0/#torch>=1.7.0/' requirements.txt
-sed -i 's/^torchvision>=0.8.1/#torchvision>=0.8.1/' requirements.txt
-
-sudo apt install -y libfreetype6-dev
-
-python3 -m pip install -r requirements.txt
-
-cd ~
-
-#install PyTorch
-sudo apt-get install -y libopenblas-base libopenmpi-dev
-
-wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-
-python3 -m pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-
-sudo apt install -y libjpeg-dev zlib1g-dev
-
-#install TorchVision
-sudo apt install -y libjpeg-dev zlib1g-dev
-git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
-cd torchvision
-sudo python3 setup.py install 
+#depencencies needed for Docker to run RTSP client
+sudo apt install -y  python3-testresources gstreamer-1.0 gstreamer1.0-dev python3-gi python3-gst-1.0 libgirepository1.0-dev libcairo2-dev gir1.2-gstreamer-1.0 gir1.2-gst-rtsp-server-1.0 libcanberra-gtk-module libcanberra-gtk3-module
+#other dependencies
+sudo apt install -y libfreetype6-dev tensorrt
 
 #install DeepStream
 
 #this is for latest DeepSteam 6.2
-#sudo apt install libssl1.1 libgstreamer1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer-plugins-base1.0-dev libgstrtspserver-1.0-0 libjansson4 libyaml-cpp-dev
+#sudo apt install -y libssl1.1 libjansson4 libyaml-cpp-dev
 
 #for deepstream 6.0.1
-sudo apt install \
-libssl1.0.0 \
-libgstreamer1.0-0 \
-gstreamer1.0-tools \
-gstreamer1.0-plugins-good \
-gstreamer1.0-plugins-bad \
-gstreamer1.0-plugins-ugly \
-gstreamer1.0-libav \
-libgstrtspserver-1.0-0 \
-libjansson4=2.11-1
+sudo apt install -y libssl1.0.0 libjansson4=2.11-1
+
+sudo apt install -y libgstreamer1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstrtspserver-1.0-0 
+
 
 # -- install kafka
 git clone https://github.com/edenhill/librdkafka.git
@@ -71,9 +40,46 @@ sudo apt install --reinstall nvidia-l4t-multimedia
 sudo apt install --reinstall nvidia-l4t-core
 
 
-# install deepstream
+# install deepstream (package)
 
-sudo apt install ~/jetson-nano/pkgs/deepstream-6.0_6.0.1-1_arm64.deb
+sudo apt install /media/${USER}/drivers/deepstream-6.0_6.0.1-1_arm64.deb
+
+
+#time to yolo v5
+
+git clone https://github.com/pawangonnakuti/yolov5-jetson-nano.git yolov5
+
+cd yolov5
+
+#cp requirements.txt requirements.txt.bkup
+
+#sed -i 's/^torch>=1.7.0/#torch>=1.7.0/' requirements.txt
+#sed -i 's/^torchvision>=0.8.1/#torchvision>=0.8.1/' requirements.txt
+
+
+
+python3 -m pip install -r requirements.txt
+
+cd ~
+
+#install PyTorch
+sudo apt-get install -y libopenblas-base libopenmpi-dev libjpeg-dev zlib1g-dev libpng-dev
+
+export OPENBLAS_CORETYPE=ARMV8 | tee -e ~/.bashrc
+
+wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+
+python3 -m pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+
+sudo apt install -y 
+
+#install TorchVision
+sudo apt install -y libjpeg-dev zlib1g-dev
+git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
+cd torchvision
+sudo python3 setup.py install 
+
+
 
 
 
